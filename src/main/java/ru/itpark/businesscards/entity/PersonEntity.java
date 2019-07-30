903297604d32d9b5ffd2a39320d689d26f6bc630
ruslan.nizamov <ruslan.nizamov@rtlabs.ru>
2019-07-30 20:48:00 +0300

    replace save methods for Person

--- src/main/java/ru/itpark/businesscards/entity/PersonEntity.java
+++ src/main/java/ru/itpark/businesscards/entity/PersonEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
@@ -17,12 +18,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PersonEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    @NotNull
     private String surName;
+    @NotNull
     private String name;
+    @NotNull
     private String patrName;
     private LocalDate birthDate;
+
 }
--- src/main/java/ru/itpark/businesscards/mapper/PersonMapper.java
+++ src/main/java/ru/itpark/businesscards/mapper/PersonMapper.java
@@ -4,17 +4,19 @@ import ru.itpark.businesscards.dto.Person;
 import ru.itpark.businesscards.entity.PersonEntity;
 
 public class PersonMapper {
+
     public static Person toModel(PersonEntity entity) {
         if (entity == null) {
             return null;
         }
 
         return Person.builder()
-                .id(entity.getId())
-                .surName(entity.getSurName())
-                .patrName(entity.getPatrName())
-                .birthDate(entity.getBirthDate())
-                .build();
+                     .id(entity.getId())
+                     .surName(entity.getSurName())
+                     .name(entity.getName())
+                     .patrName(entity.getPatrName())
+                     .birthDate(entity.getBirthDate())
+                     .build();
     }
 
     public static void fillEntity(PersonEntity entity, Person model) {
@@ -27,4 +29,5 @@ public class PersonMapper {
         entity.setPatrName(model.getPatrName());
         entity.setBirthDate(model.getBirthDate());
     }
+
 }
