bd30d0b2e3446f83c81e46f36b2567283a3bcb5c
ruslan.nizamov <ruslan.nizamov@rtlabs.ru>
2019-07-31 09:42:31 +0300

    add authentication

--- src/main/java/ru/itpark/businesscards/controller/RegistrationRestController.java
+++ src/main/java/ru/itpark/businesscards/controller/RegistrationRestController.java
@@ -1,25 +1,30 @@
 package ru.itpark.businesscards.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itpark.businesscards.constant.Url;
+import ru.itpark.businesscards.dto.AuthenticationTokenRequestDto;
+import ru.itpark.businesscards.dto.AuthenticationTokenResponseDto;
 import ru.itpark.businesscards.dto.RegistrationRequestDto;
+import ru.itpark.businesscards.service.AuthenticationService;
 import ru.itpark.businesscards.service.RegistrationService;
 
 @RestController
 @RequestMapping(Url.Registration.FULL)
 @RequiredArgsConstructor
 public class RegistrationRestController {
-  private final RegistrationService service;
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void register(@RequestBody RegistrationRequestDto dto) {
-    service.register(dto);
-  }
+    private final RegistrationService registrationService;
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public AuthenticationTokenResponseDto register(@RequestBody RegistrationRequestDto dto) {
+        registrationService.register(dto);
+        return authenticationService.authenticate(new AuthenticationTokenRequestDto(dto.getPassword(),
+                                                                                    dto.getUsername()));
+    }
+
 }
